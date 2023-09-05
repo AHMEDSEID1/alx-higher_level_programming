236898cee2f161d99b046a602b86b3c682f5cba8
AHMEDSEID1 <kedi3340@gmail.com>
2023-09-05 21:42:33 +0000

    is lower

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-for number in range(100):
-    print("{:02d}".format(number), end="")
-    if number != 99:
-        print(", ", end="")
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
     else:
-        print()
+        print("{:02}, ".format(i), end="")
