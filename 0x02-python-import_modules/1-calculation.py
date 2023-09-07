79586712ec4520328e7207ee429286f0079e95ad
AHMEDSEID1 <kedi3340@gmail.com>
2023-09-07 19:25:17 +0000

    correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
-a = 1
-b = 2
+a = 10
+b = 5
+import calculator_1
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+result_add = calculator_1.add(a, b)
+result_sub = calculator_1.sub(a, b)
+result_mul = calculator_1.mul(a, b)
+result_div = calculator_1.div(a, b)
+
+print("Addition: {} + {} = {}".format(a, b, result_add))
+print("Subtraction: {} - {} = {}".format(a, b, result_sub))
+print("Multiplication: {} * {} = {}".format(a, b, result_mul))
+print("Division: {} / {} = {}".format(a, b, result_div))
