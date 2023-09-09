c026502f26bc4f6078b968ed652bd06d3388eebf
AHMEDSEID1 <kedi3340@gmail.com>
2023-09-09 19:05:33 +0000

    variable load

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    sum = 0
-    for x in range(1, len(sys.argv)):
-        sum += int(sys.argv[x])
-    print(sum)
+    from variable_load_5 import a
+
+    print(a)
