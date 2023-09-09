e0804af366715ffaf187006aa1da4b417de20ac0
AHMEDSEID1 <kedi3340@gmail.com>
2023-09-09 18:49:54 +0000

    commit correct

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-# Import the calculator_1 module
-import calculator_1 as calc
-
-# Define the variables a and b
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-
-# Call the functions from the calculator_1 module using the module alias as a prefix
-add_result = calc.add(a, b)
-sub_result = calc.sub(a, b)
-mul_result = calc.mul(a, b)
-div_result = calc.div(a, b)
-
-# Print the results using the required format
-print("{} + {} = {}".format(a, b, add_result))
-print("{} - {} = {}".format(a, b, sub_result))
-print("{} * {} = {}".format(a, b, mul_result))
-print("{} / {} = {}".format(a, b, div_result))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
