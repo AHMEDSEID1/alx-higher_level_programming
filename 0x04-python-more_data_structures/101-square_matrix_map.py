38fa1c99bd6536ee004a6c4754a27b728b82a1ec
AHMEDSEID1 <kedi3340@gmail.com>
2023-09-15 20:31:50 +0000

    complex delete

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
+
 def square_matrix_map(matrix=[]):
-    """Returns a new matrix with the square of each value"""
-    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
