57c41a5a694d9222a395f5ad0fbc068e5e0ca72d
AHMEDSEID1 <kedi3340@gmail.com>
2023-09-15 19:47:10 +0000

    a dictionary

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,11 @@
 #!/bin/usr/python3
-
 def weight_average(my_list=[]):
+    """Returns the weighted average of all integers tuple (<score>, <weight>)"""
     if not my_list:
         return 0
-
-    total_sum = 0
-    total_weight = 0
-
+    sum_prod = 0
+    sum_weight = 0
     for score, weight in my_list:
-        total_sum += score * weight
-        total_weight += weight
-
-    return total_sum / total_weight
+        sum_prod += score * weight
+        sum_weight += weight
+    return sum_prod / sum_weight
