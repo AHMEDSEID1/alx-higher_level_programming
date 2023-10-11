8c21788228ac739c634771dfede2fc5049d2b0e8
AHMEDSEID1 <kedi3340@gmail.com>
2023-10-11 21:08:29 +0000

    append after text

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def append_after(filename="", search_string="", new_string=""):
+    """
+    Appends a line of text after each line containing a specific string in a file.
+
+    Args:
+        filename (str): The name of the file to modify.
+        search_string (str): The string to search for in each line.
+        new_string (str): The line of text to append after the lines containing the search string.
+
+    """
     with open(filename, 'r+') as file:
         lines = file.readlines()
         file.seek(0)
